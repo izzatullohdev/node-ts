383520289e6cfafcf72a30b079df380e0e89a331
izzatullohdev <izzatbekmadaminov7141@gmail.com>
2025-03-07 03:25:57 +0500

    eslint typescript-eslint/no-unsafe-assignment

---
+++ eslint.config.mjs
@@ -0,0 +1,35 @@
+// @ts-nocheck
+
+import js from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: import.meta.dirname,
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    ignores: ['dist/', 'node_modules/'],
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          args: 'all',
+          argsIgnorePattern: '^_',
+          caughtErrors: 'all',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        },
+      ],
+    },
+  },
+];
